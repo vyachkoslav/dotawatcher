16c7d012a93ad517a9130041d2f99b207c770b03
Vyacheslav Denisov <vyach.denisov@gmail.com>
2024-06-02 18:23:25 +0300

    Added details of device of user

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,19 @@ use tokio::time;
 use tokio::time::Duration;
 // use serenity::prelude::*;
 
+macro_rules! get_string_for_status {
+    ($status:expr) => {
+        match $status {
+            OnlineStatus::Offline => "не в сети",
+            OnlineStatus::Idle => "спит",
+            OnlineStatus::Invisible => "в невидимке",
+            OnlineStatus::Online => "в сети",
+            OnlineStatus::DoNotDisturb => "просит не беспокоить",
+            _ => "непонятно"
+        }
+    };
+}
+
 lazy_static!{
     static ref TARGET_GUILD: u64 = env::var("TARGET_SERVER").expect("Expected TARGET_SERVER in the environment").parse().expect("TARGET_SERVER not a number");
     static ref OUTPUT_CHANNEL: u64 = env::var("OUTPUT_CHANNEL").expect("Expected OUTPUT_CHANNEL in the environment").parse().expect("OUTPUT_CHANNEL not a number");
@@ -42,7 +55,7 @@ impl EventHandler for Handler {
             let reaction = ReactionType::Custom {
                 animated: false,
                 id: EmojiId::new(*EMOJI_ID),
-                name: Some((*EMOJI_NAME).clone()) 
+                name: Some((*EMOJI_NAME).to_string()), 
             };
             if let Err(why) = msg.react(&ctx.http, reaction).await {
                 eprintln!("Error reacting to message: {why:?}");
@@ -56,42 +69,58 @@ impl EventHandler for Handler {
         }
 
         // Username is not received when user is offline, so requesting it
-        let user: Option<User> = new_data.user.id.to_user(ctx.http()).await.ok();
-        let username = if let Some(user) = user { user.name } else { "непонятно кто".to_string() };
+        let user: Option<User> = match new_data.user.id.to_user(ctx.http()).await {
+            Ok(u) => Some(u),
+            Err(err) => {
+                eprintln!("Couldn't receive user: {err:?}");
+                None
+            },
+        };
+
+        let username: &str = user.as_ref().map_or("непонятно кто", |u| &u.name);
 
         let mut message: CreateMessage = Default::default();
-        let status = match new_data.status {
-            OnlineStatus::Offline => "не в сети",
-            OnlineStatus::Idle => "спит",
-            OnlineStatus::Invisible => "в невидимке",
-            OnlineStatus::Online => "в сети",
-            OnlineStatus::DoNotDisturb => "просит не беспокоить",
-            _ => "непонятно"
-        };
+        let mut status: &str = get_string_for_status!(new_data.status);
+
+        let device = new_data.client_status.map_or("", |device| {
+            if let Some(s) = device.mobile { status = get_string_for_status!(s); "с телефона" }
+            else if let Some(s) = device.web { status = get_string_for_status!(s); "с браузера" }
+            else { status = get_string_for_status!(new_data.status); "" }
+        });
+
         if new_data.activities.is_empty() {
-            message = message.content(format!("{} теперь {}", username, status));
+            message = message.content(format!("{} теперь {} {}", username, status, device));
         } else {
-            let mut activity = new_data.activities[0].clone();
+            let activity = &new_data.activities[0];
+
+            let activity_name: &str;
+            let activity_details: &Option<String>;
             if activity.kind == ActivityType::Custom {
-                activity.name = activity.details.unwrap_or("ничего".to_string());
-                activity.details = None;
+                activity_name = activity.details.as_deref().unwrap_or_default();
+                activity_details = &None;
+            } else { 
+                activity_name = &activity.name;
+                activity_details = &activity.details;
             }
-            let large_text: String;
-            let small_text: String;
-            if let Some(assets) = activity.assets {
-                large_text = assets.large_text.unwrap_or_default();
-                small_text = assets.small_text.unwrap_or_default();
+
+            let large_text: &Option<String>;
+            let small_text: &Option<String>;
+            if let Some(assets) = activity.assets.as_ref() {
+                large_text = &assets.large_text;
+                small_text = &assets.small_text;
             } else {
-                large_text = String::default();
-                small_text = String::default();
+                large_text = &None;
+                small_text = &None;
             }
-            message = message.content(format!("{} {} и шпилит в {}\n{}\n{}\n{}", 
+
+            message = message.content(format!("{} {} {} и шпилит в {}\n{}\n{}\n{}", 
                     username, 
-                    status, 
-                    activity.name, 
-                    activity.details.unwrap_or_default(),
-                    large_text,
-                    small_text,
+                    status,
+                    device,
+                    activity_name, 
+                    activity_details.as_deref().unwrap_or_default(),
+                    large_text.as_deref().unwrap_or_default(),
+                    small_text.as_deref().unwrap_or_default(),
             ));
         }
         if let Err(why) = ChannelId::new(*OUTPUT_CHANNEL).send_message(ctx.http(), message).await {
@@ -102,7 +131,7 @@ impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
         let mut activity = ActivityData::custom("");
-        activity.state = Some((*ACTIVITY_STRING).clone());
+        activity.state = Some((*ACTIVITY_STRING).to_string());
         ctx.set_activity(Some(activity));
         // main_loop(&ctx).await
     }
